237f06bcf4c2b909641ee2df499ba3737efbf8db
Israr Ahmad Satti <162900440+IsrarAhmadSatti@users.noreply.github.com>
2024-04-06 00:12:15 +0500

    Update LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -2,11 +2,11 @@ year = int(input("Enter a year: "))
 
 if year < 1582:
 	print("Not within the Gregorian calendar period")
-elif year%4!=0:
+elif year % 4 != 0:
     print('common year')
-elif year%100!=0:
+elif year % 100 != 0:
     print('leap year')
-elif year%400!=0:
+elif year % 400 != 0:
     print('common year')
 else:
     print('leap year')
