be088f1f85ac874d34ad9ac7a10a761b9e2f9b60
Israr Ahmad <israr.ahmad870@gmail.com>
2024-04-07 00:14:05 +0500

    last

--- fabbonacci.py
+++ fabbonacci.py
@@ -6,6 +6,10 @@ def fabonacci(num):
     while b < num:
         a , b = b, a + b
         print(a , end = ' ')
+      
+try:
+    num = int(input("Enter the number to create fabbonaci series: "))
+    fabonacci(num)
 
-num = int(input("Enter the number to create fabbonaci series: "))
-fabonacci(num)
+except ValueError:
+    print("Invalid Input, please Try Again")    
